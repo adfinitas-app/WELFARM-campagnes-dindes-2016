115ac42954a8e05e21092ca1922d5a4fd6c9f43e
Arnaud Masselin <web@adfinitas.fr>
2016-11-16 14:28:11 +0000

    Updated 'js/app.js' via CloudCannon

--- js/app.js
+++ js/app.js
@@ -13,6 +13,87 @@ $(function() {
     }
   });
 });
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+function makeCorsRequest(data) {
+  var url = 'https://form-to-db.herokuapp.com/';
+  var body = JSON.stringify(data);
+  var xhr = createCORSRequest('POST', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  // Response handlers.
+  xhr.onload = function() {
+    //var text = xhr.responseText;
+    //alert('Response from CORS request to ' + url + ': ' + text);
+    document.location.href = merciPath;
+  };
+  // Error Handler
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+  xhr.send(body);
+}
+
+/*
+ * Fin de la lib
+*/
+
+function pureField(string) {
+  return (string.replace(/'/g, "%27").replace(/"/g, "&quot;"));
+}
+
+function submitForm() {
+  var infos = {};
+  /* Si il y a un champ OPTIN sur le formulaire, et qu'il n'est pas coché,
+     l'utilisateur ne doit surement pas remonter sur threads (voir avec le chef de projet).
+     Pour effectuer ça, laisser le champ threads à undefined
+   */
+  if ($("input[name='optin']:checked").length == 1) {
+    optin = true;
+  } else {
+    optin = false;
+  }
+  today = new Date();
+  var data = {
+    "schema": "welfarm",
+    "db": {
+      "firstname": pureField($("input[name='firstname']").val()),
+      "lastname": pureField($("input[name='lastname']").val()),
+      "email": pureField($("input[name='email']").val()),
+      "phone": pureField($("input[name='phone']").val()),
+      "optin": optin,
+      "event": "petitionporcinet",
+      "signin_date": today.toString()
+    }
+  }
+  makeCorsRequest(data);
+}
 
 function extractUrlParams(){
   var t = document.location.search.substring(1).split('&'); var f = [];
