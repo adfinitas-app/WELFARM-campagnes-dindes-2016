053b70bfc6877af48be9f5475aeeea3a71f9431d
Arnaud Masselin <web@adfinitas.fr>
2016-11-16 14:30:57 +0000

    Updated 'js/app.js' via CloudCannon

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 /* Smooth scroll */
+merciPath = "/merci.html"
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
